a06ac80c47da8f4e0ebb37551b38cd0557571095
Zafor Iqbal <zaforiqbal13@gmail.com>
2021-11-24 08:56:38 +0600

    corepuls contact page

---
+++ templates/contactus/contactus.php
@@ -0,0 +1,248 @@
+<?php
+
+/* 
+* Template Name: Contact us 2021
+* Developed by Zafor Iqbal 
+* www.zaforiqbal.com 
+*/
+
+?>
+
+
+<div class="desktop-view">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="contact-us-hero-text">
+              <h1>Let’s chat with our live support team</h1>
+              <p>Live Support is available</p>
+              <p>Monday-Saturday 09:00 AM - 5.00 PM (AEST)</p>
+            </div>
+            <div class="contact-hero-agents-desktop">
+              <img
+                class="agents agent1"
+                src= "<?php echo get_template_directory_uri()."/contactus/images/cca1.png"?>"
+                width="80px"
+                alt=""
+              />
+              <img
+                class="agents agent2"
+                src="<?php echo get_template_directory_uri()."/contactus/images/cca2.png"?>"
+                width="80px"
+                alt=""
+              />
+              <img
+                class="agents agent3"
+                src="<?php echo get_template_directory_uri()."/contactus/images/cca3.png"?>"
+                width="80px"
+                alt=""
+              />
+              <img
+                class="agents agent5"
+                src="<?php echo get_template_directory_uri()."/contactus/images/cca5.png"?>"
+                width="80px"
+                alt=""
+              />
+              <img
+                class="agents agent6"
+                src="<?php echo get_template_directory_uri()."/contactus/images/cca6.png"?>"
+                width="80px"
+                alt=""
+              />
+              <img
+                class="agents agent4"
+                src="<?php echo get_template_directory_uri()."/contactus/images/cca4.png"?>"
+                width="80px"
+                alt=""
+              />
+            </div>
+            <div class="contact-send-message-desktop">
+              <a href="">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Send us a
+                message &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="contact-us-gif">
+              <img src="<?php echo get_template_directory_uri()."/contactus/images/contact-us.gif" ?>" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Section -->
+    <div class="contact-phone-support">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="cc-customer cc-block">
+              <div>
+                <div>
+                  <img src="<?php echo get_template_directory_uri(). "/contactus/images/customer.svg"?>" alt="" />
+                </div>
+                <div class="cc-block-span">
+                  <span>
+                    Schedule a one-on-one demo call with our customer success
+                    experts to learn more about the coreplus basics and get
+                    started in no time.
+                  </span>
+                </div>
+              </div>
+
+              <div class="cc-block-button">
+                <a href="https://portal.coreplus.com.au/coreplusdemo"
+                  >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Schedule a Demo &#8594;
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="cc-customer cc-block">
+              <div>
+                <div>
+                  <img src="<?php echo get_template_directory_uri(). "/contactus/images/telephone.svg"?>" alt="" />
+                </div>
+                <div class="cc-block-span">
+                  <span>
+                    coreplus is here to help. Schedule phone support with our
+                    customer support agents now.
+                  </span>
+                </div>
+              </div>
+
+              <div class="cc-block-button">
+                <a href="https://portal.coreplus.com.au/coreplussupport"
+                  >Schedule Phone Support &#8594;</a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="cc-customer cc-block">
+              <div>
+                <div>
+                  <img src="<?php echo get_template_directory_uri()."/contactus/images/help-center.svg" ?>" alt="" />
+                </div>
+                <div class="cc-block-span">
+                  <span>
+                    Delve right into all the advice and answers direct from the
+                    coreplus Team.
+                  </span>
+                </div>
+              </div>
+
+              <div class="cc-block-button">
+                <a href="https://help.coreplus.com.au">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  Help Center
+                  &#8594;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Section -->
+    <div class="general-enquery">
+      <div class="container enquery-container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="enquery-content">
+              <div>
+                <p>For general enquiries Please email us</p>
+              </div>
+              <div>
+                <img src="<?php echo get_template_directory_uri(). "/contactus/images/envelope.svg" ?>" alt="" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="contact-email-address">
+              <p>
+                Customer Support:
+                <a href="mailto:support@coreplus.com.au"
+                  >support@coreplus.com.au</a
+                >
+              </p>
+              <p>
+                Accounts:
+                <a href="mailto:accounts@coreplus.com.au"
+                  >accounts@coreplus.com.au</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="enquery-container-mobile container">
+        <div>
+          <div class="enquery-mobile">
+            <div><img src="<?php echo get_template_directory_uri(). "/contactus/images/envelope.svg"?>" alt="" /></div>
+            <div class="enquery-content-mobile">
+              <p>
+                For general enquiries <br />
+                Please email us
+              </p>
+            </div>
+          </div>
+          <div class="contact-email-address-mobile">
+            <p>
+              Customer Support:
+              <a href="mailto:support@coreplus.com.au"
+                >support@coreplus.com.au</a
+              >
+            </p>
+            <p>
+              Accounts:
+              <a href="mailto:accounts@coreplus.com.au"
+                >accounts@coreplus.com.au</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Section -->
+    <div class="coreplus-contact-bottom-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="contact-last-sec-column">
+              <div class="">
+                <img src="<?php echo get_template_directory_uri(). "/contactus/images/delivery.svg" ?>" alt="" />
+              </div>
+              <div>
+                <p>Want to send us a present through mail?</p>
+                <span
+                  >P.O. BOX 348 Moonee <br />
+                  Ponds Victoria Australia, 3039</span
+                >
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="contact-last-sec-column">
+              <div class="">
+                <img src="<?php echo get_template_directory_uri(). "/contactus/images/australia.svg" ?>" alt="" />
+              </div>
+              <div>
+                <p>Servicing All Of Australia</p>
+                <span class="servicing-all-australia">
+                  Adelaide, Canberra, Hobart, Melbourne, Sydney, Brisbane,
+                  Darwin and Perth
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<!--  -->
+
+
